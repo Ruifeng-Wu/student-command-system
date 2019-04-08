b567604f5de60c7c2b247465df6e3d65548b0922
Ruifeng-Wu <296078293@qq.com>
2019-04-08 14:00:57 +0800

    feat:Router

---
+++ src/main/java/com/tw/Router.java
@@ -0,0 +1,33 @@
+package com.tw;
+
+import com.tw.command.Command;
+import com.tw.command.impl.AddStudentInfoCommand;
+import com.tw.command.impl.ExitCommand;
+import com.tw.command.impl.HomeCommand;
+import com.tw.command.impl.ScoreSheetPrintCommand;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ruifeng-Wu
+ * @Email 296078293@qq.com
+ * @date 2019/4/7 10:04
+ */
+public class Router {
+    private Map<Integer, Command> pathMap;
+    private Command command;
+
+    public Router() {
+        pathMap = new HashMap<>();
+        pathMap.put(0, new HomeCommand());
+        pathMap.put(1, new AddStudentInfoCommand());
+        pathMap.put(2, new ScoreSheetPrintCommand());
+        pathMap.put(3,new ExitCommand());
+    }
+
+    public String route(Integer i) {
+        command = pathMap.get(i);
+        return command.invoke();
+    }
+}
