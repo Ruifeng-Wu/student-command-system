43770c25b87727c26b56ae17f95b4c042e6dc927
Ruifeng-Wu <296078293@qq.com>
2019-04-08 13:59:34 +0800

    feat:ScoreSheet

---
+++ src/main/java/com/tw/Pojo/ScoreSheet.java
@@ -0,0 +1,68 @@
+package com.tw.Pojo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Ruifeng-Wu
+ * @Email 296078293@qq.com
+ * @date 2019/4/8 19:10
+ */
+public class ScoreSheet {
+    private List<Student> studentList;
+    private double totalAverage;
+    private double totalMedian;
+
+    public ScoreSheet() {
+        studentList = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        studentList.add(student);
+        summary();
+    }
+
+    public void summary() {
+        double[] totalScores = studentList.stream().mapToDouble(Student::getTotal).toArray();
+        double amount = Arrays.stream(totalScores).sum();
+        totalAverage=amount/totalScores.length;
+        Arrays.sort(totalScores);
+        int index=totalScores.length/2>0?totalScores.length/2:0;
+        totalMedian=(totalScores.length%2==0)?(totalScores[index]+totalScores[index-1])/2:totalScores[index];
+    }
+
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public double getTotalAverage() {
+        return totalAverage;
+    }
+
+    public void setTotalAverage(double totalAverage) {
+        this.totalAverage = totalAverage;
+    }
+
+    public double getTotalMedian() {
+        return totalMedian;
+    }
+
+    public void setTotalMedian(double totalMedian) {
+        this.totalMedian = totalMedian;
+    }
+
+    @Override
+    public String toString() {
+        return "ScoreSheet{" +
+                "studentList=" + studentList +
+                ", totalAverage=" + totalAverage +
+                ", totalMedian=" + totalMedian +
+                '}';
+    }
+}
